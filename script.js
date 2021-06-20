036d4ebda7dea5d5fd7bf00dce053a6c907556cb
Lauren Goss <laurenlgoss98@gmail.com>
2021-06-20 17:59:01 -0400

    added arrays for all possible characters

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function writePassword() {
     var isUppercase = getUppercaseInput();
     var isNumeric = getNumericInput();
     var isSpecial = getSpecialInput();
+
+    var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var numericArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var specialArray = [" ", "!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ",", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", "\\", "\"", "\'"];
+    
     return;
   }
 
